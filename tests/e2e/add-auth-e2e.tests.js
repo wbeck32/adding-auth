c7df2264f9706d201d53623d036197bec4c59f08
Wendy Beck <webeck@gmail.com>
2017-08-02 21:24:47 -0700

    in progresss

--- tests/e2e/add-auth-e2e.tests.js
+++ tests/e2e/add-auth-e2e.tests.js
@@ -1,29 +1,21 @@
 const req = require('../helpers/request');
 const chai = require('chai');
 const assert = chai.assert;
+const seedPeople = require('../helpers/test-data');
 
 describe('user security API', () => {
   let token = '';
 
   before(() => {
-    return req
-      .post('/users/signup')
-      .send({
-        email: 'me@me.com',
-        password: 'abc',
-        roles: [{ admin: 0, manager: 1, peon: 1 }]
-      })
-      .then(res => {
-        token = res.body.token;
-      });
+    return req.post('/users/signup').send(seedPeople[0]).then(res => {
+      token = res.body.token;
+    });
   });
 
-  it('creates a token', () => {
-    // console.log('token: ', token);
-  }), it('validates a user creating an account then adds them', () => {
-    //check for unique user name/email
-    //sends user info in payload
-    //saves token
+  it('creates a user with roles', () => {
+    return req.post('/users/signup').send(seedPeople[1]).then(res => {
+      token = res.body.token;
+    });
   }), it('checks credentials when a user tries to sign in then retrieves the user', () => {
     //retrieves token - from db? from local machine?
   }), it('gets all users', () => {
--- tests/helpers/test-data.js
+++ tests/helpers/test-data.js
@@ -1,39 +1,12 @@
-  module.exports = [{
-          "name": "elizabeth",
-          "nose": "tiny",
-          "heightInInches": 50,
-          "likesRollerCoasters": true
-      },
-      {
-          "name": "joann",
-          "job": "web dev"
-      },
-      {
-          "name": "elton",
-          "talent": "piano",
-          "likesRollerCoasters": null
-      },
-      {
-          "name": "oliver",
-          "quirks": [{
-                  "annoying": "clips toenails on transit"
-              },
-              {
-                  "healthy": "always buys rounds"
-              }
-          ]
-      },
-      {
-          "name": "james",
-          "hobby": "collects subway tokens from around the world",
-          "heightInInches": 64
-      },
-      {
-          "name": "larry",
-          "quirks": [{
-              "odd": "starred in a cheesy computer game in the 80s"
-          }],
-          "likesRollerCoasters": true,
-          "heightInInches": 64
-      }
-  ]
+module.exports = [
+  {
+    email: 'me@me.com',
+    password: 'abc',
+    roles: []
+  },
+  {
+    email: 'email@example.com',
+    password: 'password',
+    roles: [{ admin: 0, manager: 1, peon: 1 }]
+  }
+];
